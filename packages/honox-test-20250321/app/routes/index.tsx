4f0fd7c4554bbcc60b19d6e7346edc3764f11762
calloc134 <115209243+calloc134@users.noreply.github.com>
2025-03-21 16:30:15 +0900

    メッセージ変更

--- packages/honox-test-20250321/app/routes/index.tsx
+++ packages/honox-test-20250321/app/routes/index.tsx
@@ -2,7 +2,7 @@ import { createRoute } from "honox/factory";
 
 export default createRoute((c) => {
   // HonoXの場合は、このログがCloudflare側に出力されない
-  console.log("Hello from the index route!");
+  console.log("Hello HonoX!");
   return c.render(
     <div>
       <h1 class="text-3xl font-bold underline">Hello!</h1>
